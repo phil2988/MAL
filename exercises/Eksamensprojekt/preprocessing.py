8a63cf77f3b1f86612268298ae64a77ecdc0d78e
phil2988 <phillipblicher@gmail.com>
2022-12-02 10:10:10 +0100

    fix

--- exercises/Eksamensprojekt/preprocessing.py
+++ exercises/Eksamensprojekt/preprocessing.py
@@ -1,6 +1,5 @@
 import string
 from urllib.parse import quote_plus
-import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import pandas as pd
@@ -16,10 +15,14 @@ def getAllData():
             results.append(row)
     return results
 
-def getCardsAsDataFrame(data=None):
-    print("Converting data into card objects...")
-    if data == None:
-        data = getAllData()
+
+def getCardsAsDataFrameByPath(path):
+    data = []
+    with open(path, encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile)  # change contents to floats
+        for row in reader:  # each row is a list
+            data.append(row)
+
     cards = []
     for i in data[1:]:
         card = {}
@@ -27,18 +30,30 @@ def getCardsAsDataFrame(data=None):
         for k in i:
             card.update({data[0][j]: k})
             j += 1
-        print()
         cards.append(card)
     print("Done! Returning data as DataFrame\n")
+    return pd.DataFrame(data=cards)
+
+
+def getCardsAsDataFrame():
+    import os
+
+    print("Converting data into card objects...")
+    dataWithLabels = os.walk("labels")
+    cards = []
+    for i, entry in dataWithLabels:
+
+        print("Done! Returning data as DataFrame\n")
     return pd.DataFrame(data=cards)
 
 
 def removeNonUnits(cards):
     assert isinstance(cards, pd.DataFrame)
     print("Removing cards which are not a unit...")
-    
-    units = cards.drop(cards[cards["type"] != "Unit"].index) 
-    
+
+    # check if collectible
+    units = cards.drop(cards[cards["type"] != "Unit"].index)
+
     print("Done!\n")
     return units
 
@@ -70,15 +85,16 @@ def headerToIndex(label: string, data=None):
         return getHeaders()[0].index(label)
     return data[0].index(label)
 
+
 def onlyCostAttackAndHealth(units):
     print("Isolating cost, attack and health...")
-    
+
     for i in units.columns:
         if i != "cost" and i != "attack" and i != "health":
             units = units.drop(i, axis=1)
-    
+
     print("Done!\n")
-    
+
     print("Converting string values to int values...")
 
     units["attack"] = units["attack"].astype(int)
@@ -89,6 +105,7 @@ def onlyCostAttackAndHealth(units):
 
     return units
 
+
 def getTrainTestSplit_test(units):
     from sklearn.model_selection import train_test_split
 
@@ -97,9 +114,7 @@ def getTrainTestSplit_test(units):
     print("Done!\n")
 
     print("Splitting X and y into train-test split...")
-    X_train, X_test, y_train, y_test = train_test_split(
-        units, labels
-    )
+    X_train, X_test, y_train, y_test = train_test_split(units, labels)
     print("Done! Returning split as numpy arrays!\n")
 
     X_train = np.array(X_train)
